d251ab922462a0e4d17d63c4f77174662c59ccec
prashant kumar <prashantdoa1996@gmail.com>
2023-10-11 17:01:55 +0530

    array reversing without using new array

--- reversingArray.js
+++ reversingArray.js
@@ -1,1 +1,24 @@
-console.log("hi i am prashant and i want to reverse the array")
+console.log("hi i am prashant and i want to reverse the array,and no new array");
+
+function swap(arr, i, j) {
+    // function swaps the element of the array at index i and j.
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
+
+let arr = [5, 9, 1, 3, 2, 7]; // array which is to be reversed.
+
+let i = 0; // first index.
+let j = arr.length - 1; // last index.
+
+while (i <= j) {
+    swap(arr, i, j);
+    i++;
+    j--;
+};
+
+console.log(arr);
+
+
+
