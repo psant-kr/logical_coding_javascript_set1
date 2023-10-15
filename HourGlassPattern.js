ef65a32f803b030d9a453a133aeaba49d1924c21
prashant kumar <prashantdoa1996@gmail.com>
2023-10-15 10:22:47 +0530

    solution for hour glass added

--- HourGlassPattern.js
+++ HourGlassPattern.js
@@ -1,4 +1,4 @@
-Input : rows_no = 7
+// Input : rows_no = 7
 // Output :
 
 // 1 2 3 4 5 6 7
@@ -14,3 +14,37 @@ Input : rows_no = 7
 //   3 4 5 6 7
 //  2 3 4 5 6 7
 // 1 2 3 4 5 6 7
+
+function pattern(n) {
+    for (let row = 1; row <= n; row++) {
+        let str = "";
+        let spaces = row - 1;
+
+        for (let i = 1; i <= spaces; i++) {
+            str += " ";
+        }
+        let num = row;
+        while (num <= n) {
+            str += num + " ";
+            num++
+        }
+        console.log(str);
+    }
+
+    // lower part
+    for (let row = 1; row <= n - 1; row++) {
+        str = '';
+        let spaces = n - row - 1;
+        for (let i = 1; i <= spaces; i++) {
+            str += " ";
+        }
+        let num = n - row;
+        while (num <= n) {
+            str += num + " ";
+            num++;
+        }
+        console.log(str);
+    }
+}
+
+pattern(7);
