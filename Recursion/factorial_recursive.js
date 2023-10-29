053010ff7da1ead61861189011b38f7bfe3a6dc8
prashant kumar <prashantdoa1996@gmail.com>
2023-10-29 19:33:33 +0530

    factorial by loops

--- Recursion/factorial_recursive.js
+++ Recursion/factorial_recursive.js
@@ -1,9 +1,24 @@
 // function for finding the factorial of a given integers n
-function f(n){
-    if(n == 1){
+
+function f(n) {
+    // base case
+    if (n == 1) {
         return 1;
     }
-    return n * f(n-1);
+    return n * f(n - 1);
 }
 
-console.log(f(7));
+console.log(f(5));
+
+
+// loops
+/** 
+function factorialWithLoops(n) {
+    let ans = 1;
+    for (let i = 1; i <= n; i++) {
+        ans = ans * i;
+    }
+    return ans;
+}
+console.log(factorialWithLoops(7));
+*/
