2ead81cb49e9b723ab324617b64ea4408724e121
prashant kumar <prashantdoa1996@gmail.com>
2023-10-31 20:09:17 +0530

    no consecutive ones

---
+++ Recursion/noConsecutiveOnes.js
@@ -0,0 +1,17 @@
+/**
+given a +ve  value (>0) n. count the no of binary strings
+( strings which got only 0 or 1 ) of length n, such that there are 
+no consecutive ones.
+
+ex- n = 3 ans is 5.
+
+*/
+
+function  noConsicutiveOnes(n){
+    if(n == 1) return 2;
+    if(n == 2) return 3;
+    if(n > 2){
+        return noConsicutiveOnes(n -1) + noConsicutiveOnes( n -2);
+    }
+}
+console.log(noConsicutiveOnes(6));
